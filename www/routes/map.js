e157036e137107bdf33862bc2c0c6e23594eeaad
projooni@gmail.com <ljs79337A@>
2017-06-07 23:45:11 +0900

    지도 , 지도 데이터(json) 추가

--- www/routes/map.js
+++ www/routes/map.js
@@ -1,9 +1,14 @@
+/**
+ * Created by projo on 2017-06-07.
+ */
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+    res.render('map', { title: 'Map' });
+
 });
 
 module.exports = router;
